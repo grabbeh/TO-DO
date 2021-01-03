50c6b28f69fed9f524bef877bccc8710f473e02e
Michael <mbg@outlook.com>
2021-01-03 17:37:49 +0000

    svg

--- pages/todos/[id].js
+++ pages/todos/[id].js
@@ -167,7 +167,11 @@ const Todo = ({ todo, updateTodo }) => {
 }
 
 const Dustbin = () => (
-  <svg xmlns='http://www.w3.org/2000/svg' fill='currentColor'>
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 20 20'
+    fill='currentColor'
+  >
     <path
       fillRule='evenodd'
       d='M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z'
@@ -177,13 +181,21 @@ const Dustbin = () => (
 )
 
 const Edit = () => (
-  <svg xmlns='http://www.w3.org/2000/svg' fill='currentColor'>
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 20 20'
+    fill='currentColor'
+  >
     <path d='M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z' />
   </svg>
 )
 
 const CommentsIcon = () => (
-  <svg xmlns='http://www.w3.org/2000/svg' fill='currentColor'>
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 20 20'
+    fill='currentColor'
+  >
     <path
       fillRule='evenodd'
       d='M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z'
