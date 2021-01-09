f4c35257924ea44992a8c7025e792d3cfb6f24b4
Michael <mbg@outlook.com>
2021-01-09 00:02:18 +0000

    Colour

--- components/mainContainer.js
+++ components/mainContainer.js
@@ -1,7 +1,7 @@
 import { Toaster } from 'react-hot-toast'
 
 const Container = ({ children }) => (
-  <div className='bg-gray-100 h-screen pb-5 font:sans flex justify-center'>
+  <div className='pb-5 font:sans flex justify-center'>
     <Toaster position='top-right' />
     <div className='px-3 lg:px-0 h-full relative w-full lg:w-2/5'>
       {children}
