e154d2fd908b89fee6a0b1b9c7c5a1fe7471c83d
Michael <mbg@outlook.com>
2021-02-24 11:38:47 +0000

    Update

--- components/todoLists.js
+++ components/todoLists.js
@@ -13,9 +13,9 @@ const TodoLists = ({ todoLists }) => {
     setModalIsOpen(false)
   }
   return (
-    <div className='px-2 md:sticky w-full md:w-60 flex-none  h-full md:h-screen top-0 bg-blue-700'>
+    <div className='px-2 md:sticky w-full md:w-56 flex-none h-full md:h-screen top-0 bg-blue-700'>
       <h1 className='text-white text-xl my-2 font-semibold'>Lists</h1>
-      <ul>
+      <ul className='mb-2'>
         {todoLists.map(todoList => (
           <TodoList key={todoList.id} todoList={todoList} />
         ))}
