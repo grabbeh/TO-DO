875d8b26af3b46f0130b40e1d02fd154991ba221
Michael <mbg@outlook.com>
2021-03-02 17:14:49 +0000

    Update todoLists.js

--- components/todoLists.js
+++ components/todoLists.js
@@ -24,7 +24,7 @@ const TodoLists = ({ getTodos }) => {
     setModalIsOpen(false)
   }
   return (
-    <div className='px-2 md:sticky w-full md:w-56 flex-none h-full md:h-screen top-0 bg-blue-700'>
+    <div className='hidden md:inline-block px-2 md:sticky w-full md:w-56 flex-none h-full md:h-screen top-0 bg-blue-700'>
       <h1 className='text-white text-xl my-2 font-semibold'>Lists</h1>
       <ul className='mb-2'>
         <div
