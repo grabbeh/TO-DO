0230ad2dc41b1bd444151359d72ff0b38f7d78f2
Michael <mbg@outlook.com>
2021-03-19 15:24:41 +0000

    Update cardListItem.js

--- components/cardListItem.js
+++ components/cardListItem.js
@@ -1,5 +1,5 @@
 const CardListItem = ({ children, className }) => (
-  <li className={`${className} bg-white pl-2 pr-4 py-1`}>{children}</li>
+  <li className={`${className} bg-white pl-2 pr-4`}>{children}</li>
 )
 
 export default CardListItem
